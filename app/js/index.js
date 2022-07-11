49c61a56b1ca5b3b1a75eba356a2994253a71d8b
sdelrive <sdelrive97@gmail.com>
2022-07-11 14:14:07 -0300

    header retoque 1

---
+++ app/js/index.js
@@ -0,0 +1,19 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 50;
+      if (elementTop < windowHeight) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+
+  window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
